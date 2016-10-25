b34394db4ff4843d31625eb95f4a4f9c2578b6b2
Tijn Kersjes <tijn@divbyzero.nl>
2016-10-25 10:54:53 +0200

    Inverted if condition

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-"use strict";
+if (typeof window === "undefined") {
+    const window = require("./window");
 
-if (typeof window !== "undefined")
-    return;
-
-const window = require("./window");
-
-global.window = window;
-Object.keys(window).forEach(key => {
-    global[key] = window[key];
-});
+    global.window = window;
+    Object.keys(window).forEach(key => {
+        global[key] = window[key];
+    });
+}
