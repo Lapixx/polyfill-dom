f32b7d2c01261391b02742b23c0e5de889e1d1e2
Tijn Kersjes <tijn@divbyzero.nl>
2016-10-25 10:50:48 +0200

    Remove object spread operator

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ if (typeof window !== "undefined")
 
 const window = require("./window");
 
-global = { ...global, window, ...window };
+global.window = window;
+Object.keys(window).forEach(key => {
+    global[key] = window[key];
+});
